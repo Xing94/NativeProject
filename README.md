0a2a637c8b6385a94658806221573d734b27528c
Xing94 <1115119438@qq.com>
2020-06-24 11:28:36 +0800

    添加说明文件

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-##NativeProject介绍：##
-##本项目为Android引用C、rust等语言的示例；##
+#NativeProject
+[rust 地址](http)
--- module_native_rust/src/main/java/com/lxx/nativerust/FilecoinBlsSignUtil.java
+++ module_native_rust/src/main/java/com/lxx/nativerust/FilecoinBlsSignUtil.java
@@ -11,6 +11,10 @@ public class FilecoinBlsSignUtil {
 
     public static native String filPrivateKeyPublicKey(String privateKey);
 
+    /**
+     * cid的方式与Secp256k1签名方式一致，需将cid的字节数组转换为hex值，传入进来，
+     * 输出值为已经hex的签名的值，最后需要将其转换为base64，放到message中，传入接口
+     */
     public static native String filPrivateKeySign(String privateKey, String message);
 //    public static native String filPrivateKeySign(byte[] privateKey, String message);
 
